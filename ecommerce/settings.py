58f34c14bdaafe87729b578a2fa14660fa73eb0c
mad-jix <jackjaseelk@gmail.com>
2025-06-10 00:19:29 +0530

    Fix: static file config for Render

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,6 +132,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL='media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
